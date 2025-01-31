42506863dcee32c8a983a8161262ba3f60f79558
JoelXD18 <joelramoslazaro@gmail.com>
2025-01-31 14:14:55 +0100

    finish

--- var01.py
+++ var01.py
@@ -8,3 +8,25 @@ print("vFloat: ", vf)
 print("vChar: ", vc)
 print("vString: ", vs)
 print("vBoolean: ", vb)
+
+#cast: conversion de tipo de datos
+print('vi: ' + str(vi)) #str convierte en string el valor entero de vi
+vc = '2' #Una variable puede cambiar de valor durante la ejecucion
+print('vc:' + vc )
+vci = int(vc)
+print('type vc: ', type(vc))
+print('type vci: ', type(vci))
+#Python asignacion dinamica de tipos
+vci = True
+print('type vci:', type(vci))
+
+#maneras de hacer print
+print("vi:", vi, "vf:", vf,"vb:", vb)
+print(f"vi: {vi}, vf {vf}, vb {vb}")
+
+#Expresiones
+a=2
+b=3
+c= a + b
+print("c: ", c)
+c = 2 * a
